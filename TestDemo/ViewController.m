b989f1ff68de9d9f224d6bb6d49119cc0e162792
ForgetAndRemember <1965584294@qq.com>
2018-12-11 16:56:13 +0800

    Bugs

--- TestDemo/ViewController.m
+++ TestDemo/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"firstOne");
+    NSLog(@"develep_wangxu_bugs");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
